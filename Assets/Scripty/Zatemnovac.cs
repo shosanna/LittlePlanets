4b3e7c6653383a5074c407e7a87bb3574d80bac6
Zuzana Dostalova <dostalovaz@gmail.com>
2017-10-03 00:48:25 +0200

    Add simple night shader

---
+++ Assets/Scripty/Zatemnovac.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Zatemnovac : MonoBehaviour
+{
+    public Material Material;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (Material != null)
+            Graphics.Blit(src, dst, Material);
+    }
+}
