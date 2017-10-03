874c9396631f1353daf7e1626787a5ea28a27abc
Zuzana Dostalova <dostalovaz@gmail.com>
2017-10-04 00:44:11 +0200

    UI for day / night cycle

--- Assets/Scripty/GameState.cs
+++ Assets/Scripty/GameState.cs
@@ -4,15 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameState : MonoBehaviour {
+    // Core
+    private static GameState _instance = null;
+
+    // Tutorial
     public bool RunTutorial = true;
+
+    // Zvuk brany
     public bool BranaUspesneVytocena = false;
 
-    private static GameState _instance = null;
+    // Casovac
     private float _ubehlyCas = 0f;
-    private float _odpoledne = 6f;
-    private float _vecer = 8f;
-    private float _noc = 10f;
     private float _delkaDne = 12f;
+
+
+    // UI Casovac
+    private GameObject _rucickaCasovace;
+    private float _horniMezRucicky = 349f;
+    private float _dolniMezRucicky = 222f;
+    private float _rozsahRucicky;
+    private float _procentoDne = 0f;
 
     public static GameState Instance
     {
@@ -27,7 +38,9 @@ public class GameState : MonoBehaviour {
         }
 
         GetComponent<AudioSource>().Play();
-	}
+        _rucickaCasovace = GameObject.FindGameObjectWithTag("RucickaCasovace");
+        _rozsahRucicky = _horniMezRucicky - _dolniMezRucicky;
+    }
 
     void Awake()
     {
@@ -45,25 +58,36 @@ public class GameState : MonoBehaviour {
 
     private void Update()
     {
-        _ubehlyCas += Time.deltaTime;
 
-        if (_ubehlyCas >= _noc)
+        // Casovac
+        _ubehlyCas += Time.deltaTime;
+        _procentoDne = (float)Math.Round(_ubehlyCas / _delkaDne, 2);
+
+        _rucickaCasovace.transform.localPosition = new Vector3(_rucickaCasovace.transform.localPosition.x,
+                                                _horniMezRucicky - (_procentoDne * _rozsahRucicky),
+                                                _rucickaCasovace.transform.localPosition.z);
+ 
+        if (_procentoDne >= 0.9f)
         {
-            Camera.main.GetComponent<Zatemnovac>().Material.SetFloat("_Magnitude", 0.2f);
-        } else if (_ubehlyCas >= _vecer)
+            Camera.main.GetComponent<Zatemnovac>().Material.SetFloat("_Magnitude", 0.4f);
+        } else if (_procentoDne >= 0.75f)
         {
-            Camera.main.GetComponent<Zatemnovac>().Material.SetFloat("_Magnitude", 0.5f);
-        } else if (_ubehlyCas >= _odpoledne)
+            Camera.main.GetComponent<Zatemnovac>().Material.SetFloat("_Magnitude", 0.65f);
+        } else if (_procentoDne >= 0.6f)
         {
-            Camera.main.GetComponent<Zatemnovac>().Material.SetFloat("_Magnitude", 0.7f);
+            Camera.main.GetComponent<Zatemnovac>().Material.SetFloat("_Magnitude", 0.85f);
         } else
         {
             Camera.main.GetComponent<Zatemnovac>().Material.SetFloat("_Magnitude", 1f);
         }
-
-        if (_ubehlyCas >= _delkaDne)
+        
+        // Konec dne!
+        if (_procentoDne >= 1)
         {
             _ubehlyCas = 0;
+            _rucickaCasovace.transform.localPosition = new Vector3(_rucickaCasovace.transform.localPosition.x,
+                                                   _horniMezRucicky,
+                                                   _rucickaCasovace.transform.localPosition.z);
         }
 
     }
