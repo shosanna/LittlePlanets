0cd50599b8602315b8fb3a60e25bb0b97d85f5ad
Zuzana Dostalova <dostalovaz@gmail.com>
2019-05-04 23:30:33 +0200

    Add simple cow

---
+++ Assets/Scripty/CowController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Coords;
+using UnityEngine;
+
+public class CowController : MonoBehaviour {
+    public PolarCoord _polarCoord;
+
+    void Start () {
+        // Radius of the planet
+        _polarCoord.R = 0.64f;
+    }
+	
+	void Update () {
+	    _polarCoord.Phi += 0.007f;
+
+        transform.localPosition = _polarCoord.ToCartesian().ToVector3();
+	    // nataceni spritu
+	    transform.rotation = Quaternion.EulerRotation(0, 0, _polarCoord.Phi - Mathf.PI / 2);
+    }
+}
